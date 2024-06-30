f8ac48fbc8e45a24c8bcf11b76110f58c5a3eedc
soup-bowl <code@soupbowl.io>
2024-06-30 19:29:22 +0000

    Groan...

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import preact from "@preact/preset-vite"
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [preact()],
-	base:'./',
+	base:'',
 	server: {
 		host: true,
 		port: 3000,
